a7876c8972492a1c39549d01f585c54999bec0d5
nikchichua <nik.chichua@gmail.com>
2021-07-25 22:59:17 +0400

    Added frontend login and register functionality and updated login.js and register.js

--- src/components/includes/NavBar.js
+++ src/components/includes/NavBar.js
@@ -1,18 +1,28 @@
-import { Navbar, Container, Nav} from "react-bootstrap";
+import { Navbar, Container, Nav, Button} from "react-bootstrap";
 import {Link} from "react-router-dom";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faBook} from "@fortawesome/free-solid-svg-icons/faBook";
-import {faBookReader} from "@fortawesome/free-solid-svg-icons/faBookReader";
-import {faSwatchbook} from "@fortawesome/free-solid-svg-icons/faSwatchbook";
-import {faUserFriends} from "@fortawesome/free-solid-svg-icons";
+import {faLock} from "@fortawesome/free-solid-svg-icons";
 import {faEdit} from "@fortawesome/free-solid-svg-icons/faEdit";
 import {faUserPlus} from "@fortawesome/free-solid-svg-icons/faUserPlus";
+import {useDispatch, useSelector} from "react-redux";
+import {logout} from "../../services";
+import {useHistory} from "react-router";
 
 const NavBar = () => {
+    const loggedIn = useSelector(state => state.auth.isLoggedIn);
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    const handleLogout = () => {
+        dispatch(logout());
+        history.push('/');
+    }
+
     return (
         <Navbar bg='dark' variant="dark" expand='lg'>
             <Container>
-                <Link className='navbar-brand' to='/' style={{display: 'flex', alignItems: 'center', gap: '10px'}}>
+                <Link className='navbar-brand' to='/home' style={{display: 'flex', alignItems: 'center', gap: '10px'}}>
                     <FontAwesomeIcon icon={faBook}/>Book Store
                 </Link>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -20,23 +30,30 @@ const NavBar = () => {
                     justifyContent: 'space-between',
                 }} id="basic-navbar-nav">
                     <Nav className='mr-auto'>
-                        <Link className='nav-link' to='/add'>
-                            Add Book
-                        </Link>
-                        <Link className='nav-link' to='/booklist'>
-                            Book List
-                        </Link>
-                        <Link className='nav-link' to='/userlist'>
-                            User List
-                        </Link>
+                        {
+                            loggedIn &&
+                            <>
+                                <Link className='nav-link' to='/add'>Add Book</Link>
+                                <Link className='nav-link' to='/booklist'>Book List</Link>
+                                <Link className='nav-link' to='/userlist'>User List</Link>
+                            </>
+                        }
                     </Nav>
-                    <Nav className='navbar-right'>
-                        <Link style={{alignItems: 'center', gap: 10}} className='nav-link' to='/register'>
-                            <FontAwesomeIcon icon={faEdit}/> Register
-                        </Link>
-                        <Link style={{alignItems: 'center', gap: 10}} className='nav-link' to='/login'>
-                            <FontAwesomeIcon icon={faUserPlus}/> Login
-                        </Link>
+                    <Nav>
+                        {
+                            !loggedIn ?
+                                <>
+                                    <Link className='nav-link' to='/register'>
+                                        <FontAwesomeIcon icon={faEdit}/> Register
+                                    </Link>
+                                    <Link className='nav-link' to='/'>
+                                        <FontAwesomeIcon icon={faUserPlus}/> Login
+                                    </Link>
+                                </> :
+                                <Button variant='secondary' onClick={handleLogout}>
+                                    <FontAwesomeIcon icon={faLock}/> Logout
+                                </Button>
+                        }
                     </Nav>
                 </Navbar.Collapse>
             </Container>
--- src/services/httpRequests.js
+++ src/services/httpRequests.js
@@ -1,2 +1,2 @@
-export const BookRequest = 'http://localhost:8080/books';
+export const BookRequest = 'http://localhost:9999/books';
 export const UserGetRequest = 'https://randomapi.com/api/6de6abfedb24f889e0b5f675edc50deb?fmt=raw&sole';
